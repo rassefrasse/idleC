b46dceeef189ac06e619868caaf7edce389bfdda
rassefrasse <rasmusekblom@gmail.com>
2025-09-03 16:33:53 +0200

    Fixed combat loop, incorporated structs and arrays

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <windows.h>
+#include <string.h>
+
+struct Monster{ ////experimenting with structs to streamline the process of creating enemies
+    int hp;
+    int atk;
+    int loot;
+    char name[20];
+};
 
 int main(){
+
+    struct Monster monster[3];
+    strcpy(monster[0].name, "Goblin");
+    strcpy(monster[1].name, "Orc");
+    strcpy(monster[2].name, "Grizzly Bear");
+
+    monster[0].hp = 10;
+    monster[1].hp = 15;
+    monster[2].hp = 15;
+
+    monster[0].atk = 1;
+    monster[1].atk = 2;
+    monster[2].atk = 4;
+
+    monster[0].loot = 5;
+    monster[1].loot = 7;
+    monster[2].loot = 10;
+
+    int playerLife = 50;
+    int playerDamage = 3;
+    int remainingHealthPlayer;
+    int remainingHealthMonster;
+
+    int playerGold = 0;
+
+    int i = 0;
 
     char initiateGame;
     
@@ -10,32 +44,20 @@ int main(){
 
     if (initiateGame == 'Y' || initiateGame == 'y'){
 
-    
-
-    int playerLife = 10;
-    int playerDamage = 2;
-    int monsterLife = 5;
-    int monsterDamage = 1;
-    int monsterGold = 3;
-    int remainingHealthPlayer;
-    int remainingHealthMonster;
-
-    int playerGold = 0;
-
-    while ((playerLife > 0) && (monsterLife > 0)){
-        playerLife = playerLife - monsterDamage;
-        monsterLife = monsterLife - playerDamage;
-        printf("You have taken %d damage and your health is now %d\n", monsterDamage, playerLife );
-        printf("The goblin has taken %d damage and their health is now %d\n", playerDamage, monsterLife );
+    while ((playerLife > 0) && (monster[i].hp > 0)){
+        playerLife = playerLife - monster[i].atk;
+        monster[i].hp = monster[i].hp - playerDamage;
+        printf("You have taken %d damage and your health is now %d\n", monster[i].atk, playerLife );
+        printf("The goblin has taken %d damage and their health is now %d\n", playerDamage, monster[i].atk );
         Sleep(1000);
     } 
     
     if (playerLife <= 0){
         printf("You've been defeated!");
-    } else if (monsterLife <= 0){
-        printf("You've defeated the goblin!\n");
-        playerGold = playerGold + monsterGold;
-        printf("You've earned %d gold! and you now have a total of %d gold!\n", monsterGold, playerGold);
+    } else if (monster[i].hp <= 0){
+        printf("You've defeated the %c!\n", monster[i].name);
+        playerGold = playerGold + monster[i].loot;
+        printf("You've earned %d gold! and you now have a total of %d gold!\n", monster[i].loot, playerGold);
         }
 
 } else {
